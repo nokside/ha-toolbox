9bb0180c58a7acfd76c7bcd3f9591b16f8afa61f
nokside <276099665+nokside@users.noreply.github.com>
2026-09-12 01:10:14 +0300

    Refactor battery percentage calculation logic
    Removed battery hysteresis logic and thresholds, and updated battery percentage calculation based on voltage limits.

--- quirks/lumi_magnet_acn001.py
+++ quirks/lumi_magnet_acn001.py
@@ -31,18 +31,10 @@ class AqaraE1LifelineCluster(LocalDataCluster):
     cluster_id = 0xFC02
     ep_attribute = "aqara_e1_lifeline"
 
-    BATTERY_HYSTERESIS_MV: Final = 10
-
-    BATTERY_PERCENTAGE_THRESHOLDS_MV: Final = (
-        (2870, 100),
-        (2840, 50),
-        (2810, 25),
-        (2790, 5),
-    )
+    MAX_VOLTS_MV: Final = 3000
+    MIN_VOLTS_MV: Final = 2600
 
     class AttributeDefs(BaseAttributeDefs):
-        """Attribute definitions."""
-
         battery_percentage: Final = ZCLAttributeDef(
             id=0x0000,
             type=t.uint8_t,
@@ -60,40 +52,22 @@ class AqaraE1LifelineCluster(LocalDataCluster):
     }
 
     def update_from_voltage(self, voltage_mv: int) -> None:
-        """Update battery voltage and estimated battery percentage."""
         self.update_attribute(
             self.AttributeDefs.battery_voltage.id,
             voltage_mv / 1000,
         )
+
+        if voltage_mv >= self.MAX_VOLTS_MV:
+            percentage = 100
+        elif voltage_mv < self.MIN_VOLTS_MV:
+            percentage = 0
+        else:
+            percentage = int(0.00045 * voltage_mv**2 - 2.277 * voltage_mv + 2880)
+
         self.update_attribute(
             self.AttributeDefs.battery_percentage.id,
-            self._battery_percentage_with_hysteresis(voltage_mv),
+            percentage,
         )
-
-    def _battery_percentage_from_voltage(self, voltage_mv: int) -> int:
-        """Estimate coarse CR1632 battery percentage from voltage."""
-        for threshold_mv, battery_percentage in self.BATTERY_PERCENTAGE_THRESHOLDS_MV:
-            if voltage_mv >= threshold_mv:
-                return battery_percentage
-
-        return 0
-
-    def _battery_percentage_with_hysteresis(self, voltage_mv: int) -> int:
-        """Estimate coarse battery percentage with two-way hysteresis."""
-        new_percentage = self._battery_percentage_from_voltage(voltage_mv)
-        cached_percentage = self.get(
-            self.AttributeDefs.battery_percentage.id,
-        )
-
-        if cached_percentage is None or new_percentage == cached_percentage:
-            return new_percentage
-
-        if new_percentage < cached_percentage:
-            voltage_mv += self.BATTERY_HYSTERESIS_MV
-        else:
-            voltage_mv -= self.BATTERY_HYSTERESIS_MV
-
-        return self._battery_percentage_from_voltage(voltage_mv)
 
 
 class AqaraE1ManufacturerCluster(CustomCluster):
