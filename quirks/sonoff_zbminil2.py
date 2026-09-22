017bca6f34681d1341ef8cb4985a33faa29af5fe
nokside <276099665+nokside@users.noreply.github.com>
2026-09-22 23:57:31 +0300

    Refactor SONOFF ZBMINIL2 quirk with new device class

--- quirks/sonoff_zbminil2.py
+++ quirks/sonoff_zbminil2.py
@@ -1,31 +1,38 @@
-"""SONOFF ZBMINI Extreme Zigbee Smart Switch ZBMINIL2."""
+"""Quirk v2 for SONOFF ZBMINIL2."""
 
 from zhaquirks.builder import QuirkBuilder
-from zigpy.zcl.clusters.general import PowerConfiguration
-from zigpy.zdo.types import NodeDescriptor
+from zhaquirks.device import CustomZigpyDevice
+from zigpy.zcl import ClusterType
+from zigpy.zcl.clusters.general import KeepAlive, PowerConfiguration
+from zigpy.zdo.types import MACCapabilityFlags
 
 
-ZBMINIL2_NODE_DESCRIPTOR = NodeDescriptor(
-    logical_type=2,
-    complex_descriptor_available=0,
-    user_descriptor_available=0,
-    reserved=0,
-    aps_flags=0,
-    frequency_band=8,
-    mac_capability_flags=132,  # Fix mains power source.
-    manufacturer_code=4742,
-    maximum_buffer_size=82,
-    maximum_incoming_transfer_size=1024,
-    server_mask=11264,
-    maximum_outgoing_transfer_size=1024,
-    descriptor_capability_field=0,
-)
+class ZBMINIL2Device(CustomZigpyDevice):
+    """Set ZBMINIL2 to mains power."""
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+
+        node_desc = self.node_desc
+        if (
+            node_desc is not None
+            and node_desc.mac_capability_flags == MACCapabilityFlags.AllocateAddress
+        ):
+            self.node_desc = node_desc.replace(
+                mac_capability_flags=(
+                    MACCapabilityFlags.AllocateAddress
+                    | MACCapabilityFlags.MainsPowered
+                )
+            )
 
 
 (
     QuirkBuilder("SONOFF", "ZBMINIL2")
-    .node_descriptor(ZBMINIL2_NODE_DESCRIPTOR)
+    .zigpy_device_class(ZBMINIL2Device)
     # Remove the empty PowerConfiguration cluster to avoid a fake battery entity.
     .removes(PowerConfiguration.cluster_id, endpoint_id=1)
+    # Add the hidden KeepAlive client cluster used by ZBMINIL2.
+    .adds(KeepAlive, cluster_type=ClusterType.Client)
     .add_to_registry()
 )
